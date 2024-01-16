dbbed4d6e4e95470015808390227c89ce3475a56
Juan_C_Hernandez <33169379+JuanC-Hernandez@users.noreply.github.com>
2024-01-15 22:51:39 -0800

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_FieldsProperties_JuanHernandez
+{
+    internal class Car
+    {
+        private string _brand; // this is the make of a car, every car has a make
+        // this is prive to prevent unwanted users have access to this information.
+
+        // Public property for the _brand field with both getter and setter
+        public string Brand
+        {
+            get { return _brand; } 
+            set
+            {
+            
+                if (!string.IsNullOrEmpty(value))
+                    _brand = value;
+                else
+                    throw new ArgumentException("Name cannot be empty");
+            }
+        }
+        // Encapsulation makes field eaiser to modify without affecting other parts.
+        public string ReadOnlyBrand //only getter
+        {
+            get { return _brand; }
+        }
+
+        public string WriteOnlyBrand //only setter
+        {
+            set { _brand = value; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace GA_FieldsProperties_JuanHernandez
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Car reserved_space1 = new Car();
+
+            reserved_space1.Brand = "Toyota";
+
+            Console.WriteLine(reserved_space1.Brand);
+
+            Console.WriteLine(reserved_space1.ReadOnlyBrand);
+
+            reserved_space1.WriteOnlyBrand = "Honda";
+
+            Console.WriteLine(reserved_space1.Brand);
+        }
+    }
+}
